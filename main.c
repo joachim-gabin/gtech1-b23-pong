739276d6b49a58b43e6cd1ccf1cc6dff7770d7fc
WIART Noah <nwiart@gaming.tech>
2021-10-22 10:23:02 +0200

    I made a window (not a lot of progress)

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <SDL2/SDL.h>
 
+SDL_Window* window = 0;
+
 int main()
 {
 	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-		printf( "Init failed.\n" );
+		printf( "SDL init failed.\n" );
+		return 1;
 	}
+
+	window = SDL_CreateWindow( "PONG!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN );
+	if ( !window )
+	{
+		printf( "SDL window creation failed.\n" );
+		return 2;
+	}
+
+	SDL_Delay( 3000 );
+	return 0;
 }
