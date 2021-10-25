f5b55a093c6c94e801a3a9bf923f1c18ab38a220
WIART Noah <nwiart@gaming.tech>
2021-10-25 08:40:32 +0200

    Added run script + Some comments in main.c

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include <SDL2/SDL.h>
 
 SDL_Window* window = 0;
 
 int main()
 {
+	// Initialize SDL.
 	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
 		printf( "SDL init failed.\n" );
 		return 1;
 	}
 
+	// Create the SDL window.
 	window = SDL_CreateWindow( "PONG!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN );
 	if ( !window )
 	{
@@ -17,6 +22,16 @@ int main()
 		return 2;
 	}
 
-	SDL_Delay( 3000 );
+	// Wait 3 seconds then exit the game.
+	for ( int i = 1; i <= 3; ++i )
+	{
+		SDL_Delay( 1000 );
+		printf( "%i\n", i );
+	}
+
+	SDL_DestroyWindow( window );
+
+	SDL_Quit();
+
 	return 0;
 }
