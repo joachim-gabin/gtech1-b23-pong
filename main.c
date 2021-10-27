57477c5f60ffb5f684c8fecaf6fec1e8a50fc7ed
gabin <ejoachimgabin@gaming.tech>
2021-10-27 11:25:55 +0200

    Add player 1 and 2, delimiteur, and color background

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -5,7 +7,7 @@
 
 #include "pong.h"
 #include "ball.h"
-
+#include "player.h"
 
 
 SDL_Window* window = 0;
@@ -13,11 +15,15 @@ SDL_Renderer* renderer = 0;
 
 int  init( void );
 void quit( void );
-
+player player1;
+player player2;
 
 
 int main()
 {
+	player1.posY = 0;
+	player2.posY = 0;
+
 	int initCode = init();
 	if ( initCode )
 		return initCode;
@@ -29,7 +35,7 @@ int main()
 	ball_t ball;
 	ball_init( &ball, 5 );
 
-	Uint32 frameStart, frameTime, frameDelay = 10;
+	Uint32 frameStart, frameTime, frameDelay = 20;
 
 	while (!closeRequest){
 
@@ -47,14 +53,43 @@ int main()
 				}
 			}
 		}
+		const Uint8*keystates = SDL_GetKeyboardState(0);
+
+		if(keystates[SDL_SCANCODE_W]){
+			player1.posY -= 7;
+			if(player1.posY <= 0)
+				player1.posY = 0;
+		}
+
+                if(keystates[SDL_SCANCODE_S]){
+                        player1.posY += 7;
+			if (player1.posY >= SCREEN_HEIGHT - 60)
+				player1.posY = SCREEN_HEIGHT - 60;
+		}
+
+                if(keystates[SDL_SCANCODE_UP]){
+                        player2.posY -= 7;
+			if (player2.posY <= 0)
+				player2.posY = 0;
+		}
+
+                if(keystates[SDL_SCANCODE_DOWN]){
+                        player2.posY += 7;
+			if (player2.posY >= SCREEN_HEIGHT - 60)
+				player2.posY = SCREEN_HEIGHT - 60;
+		}
+
+
 
 		frameStart = SDL_GetTicks();
 
 		ball_step_pos( &ball );
 
-		SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_SetRenderDrawColor( renderer, 143, 151, 166, 255);
 		SDL_RenderClear( renderer );
 
+		filet();
+		raquette();
 
 		SDL_Rect fillRect = { ball.posX, ball.posY, 10, 10 };
 		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF );
@@ -110,4 +145,26 @@ void quit( void )
 	SDL_DestroyWindow( window );
 
 	SDL_Quit();
+}
+
+
+void filet() {
+
+
+	SDL_Rect fillRect = {SCREEN_WIDTH/2-5,0,10,SCREEN_HEIGHT};
+	SDL_SetRenderDrawColor(renderer, 46, 107, 219, 255 );
+	SDL_RenderFillRect( renderer, &fillRect);
+}
+
+int raquette() {
+
+
+	SDL_Rect fillRaquette1 = {20,player1.posY,5,60};
+        SDL_SetRenderDrawColor(renderer, 46, 107, 219, 255 );
+        SDL_RenderFillRect( renderer, &fillRaquette1);
+
+
+	SDL_Rect fillRaquette2 = {SCREEN_WIDTH-25,player2.posY,5,60};
+        SDL_SetRenderDrawColor(renderer, 46, 107, 219, 255 );
+        SDL_RenderFillRect( renderer, &fillRaquette2);
 }
